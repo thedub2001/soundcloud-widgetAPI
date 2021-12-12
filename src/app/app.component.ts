19374cf088eb6161734b113faabc4f676b1eb7ad
thedub2001 <thedub2001@hotmail.com>
2021-12-12 14:54:08 +0100

    wrapped widgatAPI functions in windgetAPI.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
- 
+import * as widgetAPI from "./widgetAPI";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,8 +22,8 @@ export class AppComponent implements AfterViewInit {
   constructor(private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    this.onReceiveWidgetMessage();
-    this.widgetConstructor(this.scFrame,"widget1");
+    widgetAPI.onReceiveWidgetMessage(this.renderer,this.SC);
+    widgetAPI.widgetConstructor(this.scFrame,"widget1",this.SC);
     this.viewIsReady = true;
 
   }
@@ -47,77 +48,6 @@ export class AppComponent implements AfterViewInit {
     this.bindedEvent=false;
   }
 
-  onReceiveWidgetMessage() {
-    this.renderer.listen('window', 'message', (event) => {
-      
-      if (event.origin !== 'https://w.soundcloud.com') { // Warning : event.origin can change with SoundCloud updates
-      // console.log('data received from iframe with origin :', event.origin)
-      return;
-    }
-    else {
-        const widgetData = JSON.parse(event.data);
-        // console.log('data received from trusted source :', widgetData);
-
-          for (const property in this.SC) {
-            if (event.source===this.SC[property].source()) {
-              // console.log("message received from widget named :",property)
-              for (const p of this.widgetGetters) {
-                  if (p==widgetData.method) {
-                    this.SC[property][widgetData.method].result=widgetData.value;
-                    return
-                  }
-              }
-              if (widgetData.value==null) this.SC[property].event[widgetData.method].result=Date.now();
-              else this.SC[property].event[widgetData.method].result=widgetData.value;
-              return
-            }
-          }
-      
-      }
-    });
-  }
-
-  widgetConstructor(elRef : ElementRef,n : string) {
-    
-    this.SC[n]= {};
-    this.SC[n].event = {};
-    this.SC[n].source = () => {return elRef.nativeElement.contentWindow};
-    this.SC[n].load = (url : string) => {elRef.nativeElement.src = "https://w.soundcloud.com/player/?url=https://" + url + "&amp;auto_play=true"};
-
-    this.widgetFunctions.forEach(e => {
-      this.SC[n][e] = (v : number) => {
-        if (v) 
-        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '","value":' + v + '}', "https://w.soundcloud.com/player/");
-        else 
-        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '"}', "https://w.soundcloud.com/player/");
-      }
-    });
-
-    this.widgetGetters.forEach(e => {
-      this.SC[n][e] = () => {
-        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '","value":null}', "https://w.soundcloud.com/player/");
-      }
-      this.SC[n][e]["value"]="";
-      Object.defineProperty(this.SC[n][e], "result", {set: function (v) {this.value=v;this.bind(v);}});
-      this.SC[n][e]["bind"]=function () {};
-      this.SC[n][e]["unbind"]=function (v: any) {this.bind=function () {};};
-
-
-    });
-
-    this.widgetEvents.forEach(e => {
-      this.SC[n].event[e] = (b : boolean) => {
-        if (b)
-          elRef.nativeElement.contentWindow.postMessage('{"method":"addEventListener","value":"' + e + '"}', "https://w.soundcloud.com/player/");
-        else
-          elRef.nativeElement.contentWindow.postMessage('{"method":"removeEventListener","value":"' + e + '"}', "https://w.soundcloud.com/player/");
-      }
-      this.SC[n].event[e]["value"]="";
-      this.SC[n].event[e]["value"]="";
-      Object.defineProperty(this.SC[n].event[e], "result", {set: function (v) {this.value=v;this.bind(v);}});
-      this.SC[n].event[e]["bind"]=function () {};
-      this.SC[n].event[e]["unbind"]=function (v: any) {this.bind=function () {};};
-    });
-  }
+// TO DO : after changing the src of the widget, binded events stop working
 
 }
---
+++ src/app/widgetAPI.ts
@@ -0,0 +1,81 @@
+import { ElementRef, Renderer2 } from "@angular/core";
+
+export var widgetFunctions = ["play", "pause", "toggle", "seekTo", "setVolume", "next", "prev", "skip"];
+export var widgetGetters = ["getVolume", "getDuration", "getPosition", "getSounds", "getCurrentSound", "getCurrentSoundIndex", "isPaused"];
+export var widgetEvents = ["loadProgress", "playProgress", "play", "pause", "finish", "seek", "ready", "sharePanelOpened", "downloadClicked", "buyClicked", "error",];
+
+
+export function onReceiveWidgetMessage(ren:Renderer2,SC:{}) {
+    ren.listen('window', 'message', (event) => {
+      
+      if (event.origin !== 'https://w.soundcloud.com') { // Warning : event.origin can change with SoundCloud updates
+      // console.log('data received from iframe with origin :', event.origin)
+      return;
+    }
+    else {
+        const widgetData = JSON.parse(event.data);
+        // console.log('data received from trusted source :', widgetData);
+
+          for (const property in SC) {
+            if (event.source===SC[property].source()) {
+               console.log("message received from widget named :",property)
+              for (const p of widgetGetters) {
+                  if (p==widgetData.method) {
+                    SC[property][widgetData.method].result=widgetData.value;
+
+                    return
+                  }
+              }
+              if (widgetData.value==null) {SC[property].event[widgetData.method].result=Date.now();                    console.log("value :",widgetData)
+            }
+              else SC[property].event[widgetData.method].result=widgetData.value;
+              return
+            }
+          }
+      
+      }
+    });
+  }
+
+export function widgetConstructor(elRef : ElementRef,n : string, SC : {}) {
+    
+    SC[n]= {};
+    SC[n].event = {};
+    SC[n].source = () => {return elRef.nativeElement.contentWindow};
+    SC[n].load = (url : string) => {elRef.nativeElement.src = "https://w.soundcloud.com/player/?url=https://" + url + "&amp;auto_play=true"};
+
+    widgetFunctions.forEach(e => {
+      SC[n][e] = (v : number) => {
+        if (v) 
+        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '","value":' + v + '}', "https://w.soundcloud.com/player/");
+        else 
+        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '"}', "https://w.soundcloud.com/player/");
+      }
+    });
+
+    widgetGetters.forEach(e => {
+      SC[n][e] = () => {
+        elRef.nativeElement.contentWindow.postMessage('{"method":"' + e + '","value":null}', "https://w.soundcloud.com/player/");
+      }
+      SC[n][e]["value"]="";
+      Object.defineProperty(SC[n][e], "result", {set: function (v) {this.value=v;this.bind(v);}});
+      SC[n][e]["bind"]=function () {};
+      SC[n][e]["unbind"]=function (v: any) {this.bind=function () {};};
+
+
+    });
+
+    widgetEvents.forEach(e => {
+      SC[n].event[e] = (b : boolean) => {
+        if (b)
+          elRef.nativeElement.contentWindow.postMessage('{"method":"addEventListener","value":"' + e + '"}', "https://w.soundcloud.com/player/");
+        else
+          elRef.nativeElement.contentWindow.postMessage('{"method":"removeEventListener","value":"' + e + '"}', "https://w.soundcloud.com/player/");
+      }
+      SC[n].event[e]["value"]="";
+      SC[n].event[e]["value"]="";
+      Object.defineProperty(SC[n].event[e], "result", {set: function (v) {this.value=v;this.bind(v);}});
+      SC[n].event[e]["bind"]=function () {};
+      SC[n].event[e]["unbind"]=function (v: any) {this.bind=function () {};};
+    });
+  }
